fae17c949c2f9cdfc89d63606e9ef82db2d78c67
KINPEACE <nikita_1997152@mail.ru>
2022-08-17 00:46:42 +0300

    add Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,63 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите 9 чисел через запятую: ");
+string seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";
+
+string RemovingSpaces(string series)
+{
+    string seriesNew = "";
+    for(int i = 0; i < series.Length; i++)
+    {
+        if(series[i] != ' ')
+        {
+            seriesNew += series[i];
+        }
+    }
+    return seriesNew;
+}
+
+int[] ArrayOfNumbers(string seriesNew)
+{
+int[] ArrayOfNumbers = new int[1];
+int j = 0;
+for(int i = 0; i < seriesNew.Length; i++)
+{
+    string seriesNew1 = "";
+
+while(seriesNew[i] != ',' && i < seriesNew.Length)
+{
+    seriesNew1 += seriesNew[i];
+    i++;
+}
+ArrayOfNumbers[j] = Convert.ToInt32(seriesNew1);
+if(i < seriesNew.Length - 1)
+{
+    ArrayOfNumbers = ArrayOfNumbers.Concat(new int[] {0}).ToArray();
+}
+j++;
+}
+return ArrayOfNumbers;
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while(index < count)
+    {
+        Console.Write(coll[index]);
+        index++;
+        if(index < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+string seriesNew = RemovingSpaces(seriesOfNumbers);
+int[] arrayOfNumbers = ArrayOfNumbers(seriesNew);
+PrintArray(arrayOfNumbers);
