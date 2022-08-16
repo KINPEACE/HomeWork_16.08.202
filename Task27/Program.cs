df8931e44e27411c6099d2ba10aaee48c33d47ea
KINPEACE <nikita_1997152@mail.ru>
2022-08-16 23:30:49 +0300

    add Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимаетна вход число и выдает сумму цифр в числе.
+
+Console.Write("Введите двухзначное число (или больше): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for(int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
